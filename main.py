faecb40082f09e4e2d59169dd6a91c563a4d5a39
John <jvansch1@binghamton.edu>
2021-03-20 16:28:30 -0400

    Add table scraping

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 from pprint import pprint
 
-from scraper import Scraper
+from scraper import Scraper 
 
-# from bs4 import BeautifulSoup
-# import requests
+scraper = Scraper('https://scrapethissite.com/pages/forms/?q=Bruins')
 
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}    
-# page = requests.get('https://hackofalltrades.dev', headers=headers)
+results = scraper.get_table_contents()
 
-# soup = BeautifulSoup(page.content, 'html.parser')
-
-# articles = soup.find_all('article')
-
-# for article in articles:
-#     article_children = article.children
-
-#     for child in article_children:
-#         print(child)    
-
-base_url = 'http://olympus.realpython.org'
-
-scraper = Scraper('https://scrapethissite.com/pages/simple/')
-
-results = scraper.find_tags_by_name('h3')
-
-print(results)
-
-# for result in results:
-#     print(result.contents)
+pprint(results)
--- scraper.py
+++ scraper.py
@@ -22,16 +22,28 @@ class Scraper:
         page_text_without_blank_lines = page_text.strip()
         print(page_text_without_blank_lines)
 
-    def find_tags_by_name(self, tag_name):
+    def get_tags_by_name(self, tag_name):
         """
         Takes in an HTML tag name and will return a list of all the tags found on the page
 
         :param tag_name string: A string representing an HTML tag(e.g. h1)
 
         :return: A list of all the found tags on the page
-        :rtype list[bs4.element.Tag]:
+        :rtype: list[bs4.element.Tag]
         """
         return self.page.find_all(tag_name)
+
+    def get_text_content_by_name(self, tag_name):
+        """
+        Takes in an HTML tag name and will return any text inside the returned tags
+        
+        :param tag_name string: A string representing an HTML tag
+
+        :return: A list of all the text inside the found tags
+        :rtype: list[string]
+        """
+        results = self.find_tags_by_name(tag_name)
+        return [result.text.strip() for result in results]
 
     def get_all_attributes_for_tag(self, tag_name, attribute):
         """
@@ -46,13 +58,46 @@ class Scraper:
         """
         tag_to_attribute_map = {}
 
-        tags = self.find_tags_by_name(tag_name)
+        tags = self.get_tags_by_name(tag_name)
 
         for tag in tags:
-            if tag.has_attr(attribute):
-                tag_to_attribute_map[tag] = tag[attribute]
+            tag_to_attribute_map[tag] = tag.get(attribute)
 
         return tag_to_attribute_map
+
+    def get_table_contents(self):
+        """
+        Finds a table on a webpage and parses it. The return value will be
+        an array where is element is dict representing a row.
+
+        :return: A list of dicts representing each row of data. Each dict will have column
+            header as keys and the cell value as value
+        :rtype: list[dict[str, str]]
+        """
+        data = []
+        tables = self.get_tags_by_name('table')
+
+        for table in tables:
+            table_header = table.find_all('th')
+
+            # The order of columns does matter here. We will map the actual rows in the same order
+            # and assume that the data will end up in the correct spot
+            columns = [column.text.strip() for column in table_header]
+
+            rows = table.find_all('tr')
+
+            for row in rows:
+                row_data = {}
+                cells = row.find_all('td')
+
+                for index, cell in enumerate(cells):
+                    row_data[columns[index]] = cell.text.strip()
+
+                # No purpose in adding empty rows
+                if len(row_data.keys()) > 0:
+                    data.append(row_data)
+        
+        return data
 
 
     
